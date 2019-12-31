1dcaa0b2f4d85eac294fc681d5068b7f3a59244f
Jari Leppanen <baikonur@kapsi.fi>
2019-12-31 19:02:23 +0200

    just to put existing stuff to remote

--- discord_twitcher.py
+++ discord_twitcher.py
@@ -6,7 +6,8 @@ import asyncio
 import discord
 from discord.ext import commands
 
-#This file will have the discord bot functions. Look at discord.py documentation
+# This file will have the discord bot functions. Look at discord.py documentation
+# Currently absolutely barebones functionality
 
 dirname, pyfilename = os.path.split(os.path.abspath(sys.argv[0]))
 tfilename = os.path.join(dirname, 'token.txt')
@@ -21,18 +22,34 @@ async def print_users():
     while(True):
         for server in bot.guilds:
             for user in server.members:
-                print(user.activities)
-        await asyncio.sleep(1)
+                for activity in user.activities:
+                    if isinstance(activity, discord.activity.Streaming):
+                        await twitch_things(server, activity)
+        await asyncio.sleep(30)
+
+# This needs to be done in the twitch things module, when it's made properly
+async def twitch_things(server, stream):
+    print(stream.details)
+    # channel = discord.utils.get(server.text_channels, name='striimihommia')
+    # await channel.send(stream.url)
+
+def stream_embed():
+    embed = discord.Embed(title=character['proper_name'],
+            colour=0x00EAFF,
+            url=character['online_webpage'],
+            description=""
+            )
+
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
     for server in bot.guilds:
         servers.append(server.id)
+    bot.loop.create_task(print_users())
 
 @bot.command()
 async def list_servers(ctx):
     print(servers)
 
-bot.loop.create_task(print_users())
 bot.run(token)
